de860abf18ccae867d580ea858195b352c5fbf46
tap <tpadilha84@gmail.com>
2011-08-10 16:53:40 -0300

    Removed small syntax error

--- app/wsgi/auth.py
+++ app/wsgi/auth.py
@@ -36,10 +36,10 @@ class AuthMiddleware(object):
         session = environ['beaker.session']
 
         user = session.get('user', None)
-        if not user:
-            # TODO Remove this when not developing
-            #user = UserService(None).get_all().next()
-            #session['user'] = user
+        #if not user:
+        #    # TODO Remove this when not developing
+        #    user = UserService(None).get_all().next()
+        #    session['user'] = user
 
         if action_callable._authentication_required:
             if not user:
